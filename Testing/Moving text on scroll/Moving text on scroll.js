6c1aca8fc839e511a84c99b9f12717daf6b46dd9
Dan-Obu Isoboye Vincent <35153846+Isobar31@users.noreply.github.com>
2019-03-19 01:29:37 +0200

    Moving text on scroll.js

--- Testing/Moving text on scroll/Moving text on scroll.js
+++ Testing/Moving text on scroll/Moving text on scroll.js
@@ -1,3 +1,3 @@
 $(document).on('scroll', function(){
-    $('h1').css("left", Math.max(500 - 0.35*window.scrollY) + "px");
+    $('h1').css("left", Math.max(1400 - 0.35*window.scrollY, 0) + "px");
 });
